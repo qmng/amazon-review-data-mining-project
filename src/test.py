5cef32299d2f2af4d989692cb4759db8f96dfe6b
Luca Joss <Luca.Joss@etu.unige.ch>
2017-05-16 18:01:12 +0200

    Changed test.py
    test.py can be lauched from anywhere

--- src/test.py
+++ src/test.py
@@ -1,6 +1,15 @@
 import subprocess
 
 var = "likely being the best"
-t = subprocess.Popen(["perl", "../../stemmer.pl"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+source = __file__.split("/")
+if len(source) > 1:
+	path = source[0]
+	if len(source) > 2:
+		for n in range(len(source)-2):
+			path = path + "/"+source[n+1]
+	path = path+"/../../stemmer.pl"
+else:
+	path = "../../stemmer.pl"
+t = subprocess.Popen(["perl", path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 test = t.communicate(input=var)
 print(test[0].strip())
