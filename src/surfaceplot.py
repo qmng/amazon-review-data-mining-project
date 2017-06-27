8700b161a88a91358ed16daca5c241a96590134c
Luca Joss <Luca.Joss@etu.unige.ch>
2017-06-27 11:22:34 +0200

    Modified surfaceplot

--- src/surfaceplot.py
+++ src/surfaceplot.py
@@ -21,40 +21,44 @@ def getXYZ(df, colX, colY, colZ):
 	# Get data of colX and colY in list format and remove duplicate values
 	x = list(set(list(df[colX])))
 	y = list(set(list(df[colY])))
-
+	x.sort()
+	y.sort()
 	# Compute meshgrid using x and y
 	X,Y = np.meshgrid(x,y)
 
 	# Inizialise Z
 	Z = np.zeros(X.shape)
 
+
 	# For each combination (x,y) find the corresponding colZ value and add it to the matrix
 	for i in range(len(x)):
 		for j in range(len(y)):
-			print(i)
-			print(j)
-			Z[i,j] = findValue(df, colX, x[i], colY, y[j], colZ)
+			Z[j,i] = findValue(df, colX, x[i], colY, y[j], colZ)
 
 	return [X,Y,Z]
 
 def plot(X,Y,Z):
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	ax.plot_surface(X,Y,Z)
+	ax.plot_wireframe(X,Y,Z)
 	ax.set_xlabel('X')
 	ax.set_ylabel('Y')
 	ax.set_zlabel('Z')
+	ax.grid(True)
 	plt.show()
 
 def main():
 	# Filtering test
-	df = pd.read_csv('pandas_demo.csv')
+	df = pd.read_csv('success_pantene.csv')
 #	print(findValue(df, 'col1', 0, 'col2', 1, 'col3')) # Print first row's value in 'col3'
 
-	[X,Y,Z] = getXYZ(df, 'col1', 'col2', 'col3')
-	print(X)
-	print(Y)
-	print(Z)
+	[X,Y,Z] = getXYZ(df, 'alpha', 'delta', 'success2')
+	# if X is logarithmic axis, for example alpha
+	X = np.log10(X)
+	plot(X,Y,Z)
+#	print(X)
+#	print(Y)
+#	print(Z)
 
 
 if __name__ == '__main__':
